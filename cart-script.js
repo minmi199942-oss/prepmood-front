c335d3352f05877903e0aed2a00dbb8194c82d58
Eunmin <minmi199942@gmail.com>
2025-10-28 01:57:20 +0900

    Fix cart item rendering - use item_id instead of id

--- cart-script.js
+++ cart-script.js
@@ -90,7 +90,7 @@ async function renderCartItems() {
   // 장바구니 아이템 렌더링
   if (cartItemsContainer) {
     cartItemsContainer.innerHTML = cartItems.map(item => `
-      <div class="cart-item" data-item-id="${escapeHtml(item.id)}">
+      <div class="cart-item" data-item-id="${escapeHtml(item.item_id)}">
         <img src="${escapeHtml(item.image)}" alt="${escapeHtml(item.name)}" class="cart-item-image" onerror="this.src='image/default.jpg'">
         <div class="cart-item-info">
           <h3 class="cart-item-name">${escapeHtml(item.name)}</h3>
