52500bfae98e721f92fb12e18d815de0fbb7dc09
Eunmin <minmi199942@gmail.com>
2026-01-11 04:38:55 +0900

    fix: deploy.sh?먯꽌 utils/checkout-utils.js 以묐났 ?⑦꽩 ?쒓굅 (utils/*.js濡??泥?

--- deploy.sh
+++ deploy.sh
@@ -110,7 +110,6 @@ rsync -av \
   --include="header.partial" \
   --include="footer.partial" \
   --include="utils.js" \
-  --include="utils/checkout-utils.js" \
   --include="common.js" \
   --include="my-*.js" \
   --include="warranty-detail.js" \
